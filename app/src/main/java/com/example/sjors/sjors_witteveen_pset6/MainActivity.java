edc53617f2b73766979c8620bc2753094c8935ad
Sjors Witteveen <sjors@jores.nl>
2016-10-19 15:07:10 +0200

    Sign in with google starts new intent; double tap back to log out

--- app/src/main/java/com/example/sjors/sjors_witteveen_pset6/MainActivity.java
+++ app/src/main/java/com/example/sjors/sjors_witteveen_pset6/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.sjors.sjors_witteveen_pset6;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    boolean doubleBackToExitPressedOnce = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            mAuth.signOut();
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Click back again to log out.", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
